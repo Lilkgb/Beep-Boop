0c6f32b5f851df8e97c37002216bf8c92dac7921
lilkgb <mcleanrs@comcast.net>
2019-01-25 11:40:28 -0800

    add function to get range of two numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
+function range(start, end){
+  if (end - start === 1) {
+    return [start + 0];
+  }else {
+    var list = range(start, end -1);
+    list.push(end -1);
+    return list;
+  }
+}
+console.log(range(49,200));
 $(document).ready(function() {
-  $("#formOne").submit(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    var input = parseInt($("#enterNumber").val());
 
   });
+  // return result;
 });
