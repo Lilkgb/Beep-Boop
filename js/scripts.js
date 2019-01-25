0633bd17f80949ae1994c82896e367c9dc2ba766
lilkgb <mcleanrs@comcast.net>
2019-01-25 11:52:01 -0800

    rang function works but not when i call a single digit. wont start at 0. attempting to debug

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
+
 function range(start, end){
   if (end - start === 1) {
     return [start + 0];
   }else {
     var list = range(start, end -1);
-    list.push(end -1);
+    list.push(end);
     return list;
   }
 }
-console.log(range(49,200));
+console.log(range(49,59));
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     var input = parseInt($("#enterNumber").val());
-
+    var result = range(input);
+    console.log(result, 23);
   });
   // return result;
 });
